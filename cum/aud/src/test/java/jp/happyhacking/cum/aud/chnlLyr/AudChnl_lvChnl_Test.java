673a00794670c8c900c0cf0a49cb7cea72006866
Takeshi Yoshida <happyhacking70@gmail.com>
2013-05-14 07:45:25 +0900

    AudChnl_lvChnl_Test

---
+++ cum/aud/src/test/java/jp/happyhacking/cum/aud/chnlLyr/AudChnl_lvChnl_Test.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package jp.happyhacking.cum.aud.chnlLyr;
+
+import static org.junit.Assert.assertEquals;
+import jp.happyhacking.cum.aud.excp.CumExcpIllegalChnlStatus;
+
+import org.junit.Test;
+
+/**
+ * @author happyhacking70@gmail.com
+ * 
+ */
+public class AudChnl_lvChnl_Test extends AudChnlTestAbst {
+
+	@Test(expected = CumExcpIllegalChnlStatus.class)
+	public void test_init() throws CumExcpIllegalChnlStatus {
+		AudChnl chnl = getChnl();
+		chnl.lvChnl();
+
+	}
+
+	@Test(expected = CumExcpIllegalChnlStatus.class)
+	public void test_joining() throws TestExcp, CumExcpIllegalChnlStatus {
+		AudChnl chnl = goto_joining();
+		chnl.lvChnl();
+	}
+
+	@Test
+	public void test_joined() throws TestExcp, CumExcpIllegalChnlStatus {
+		AudChnl chnl = goto_joined();
+		chnl.lvChnl();
+		assertEquals(AudChnl.Status.lving, chnl.chnlStatus);
+	}
+
+	@Test(expected = CumExcpIllegalChnlStatus.class)
+	public void test_rjcting() throws TestExcp, CumExcpIllegalChnlStatus {
+		AudChnl chnl = goto_rjcting();
+		chnl.lvChnl();
+	}
+
+	@Test(expected = CumExcpIllegalChnlStatus.class)
+	public void test_lving() throws TestExcp, CumExcpIllegalChnlStatus {
+		AudChnl chnl = goto_lving();
+
+		chnl.lvChnl();
+	}
+
+	@Test(expected = CumExcpIllegalChnlStatus.class)
+	public void test_clsed() throws TestExcp, CumExcpIllegalChnlStatus {
+		AudChnl chnl = goto_clsed();
+		chnl.lvChnl();
+
+	}
+
+	@Test(expected = CumExcpIllegalChnlStatus.class)
+	public void test_dscned() throws TestExcp, CumExcpIllegalChnlStatus {
+		AudChnl chnl = goto_dscned();
+		chnl.lvChnl();
+	}
+}
